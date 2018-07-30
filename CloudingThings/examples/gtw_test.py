467ec5dd2811cfd2caf1c717a74a0d5e8179213c
Jean Poma <jean@fieldcloud.com>
2018-07-30 11:17:05 +0200

    Update gtw_test.py

--- CloudingThings/examples/gtw_test.py
+++ CloudingThings/examples/gtw_test.py
@@ -20,12 +20,11 @@ if __name__ == "__main__":
     ct_gtw=ctpg.CloudingThingsPiGateway(gtw_config)
     #Actuators
 #    ct_gtw.add_actuator(CloudingThingsGroveLcd('lcd'))
-#    ct_gtw.add_actuator(CloudingThingsGroveLedbar('ledbar', pin=7))
 #    ct_gtw.add_actuator(CloudingThingsGroveOled('oled'))
-#    ct_gtw.add_actuator(CloudingThingsGroveLed('led', pin=8))
     ct_gtw.add_actuator(CloudingThingsGroveLedbar('ledbar', pin=7))
     ct_gtw.add_actuator(CloudingThingsGroveLed('led', pin=8))
-    #Sensors
+
+#Sensors
     ct_gtw.add_sensor(CloudingThingsGroveUltrasonic('ultrasonic', pin=4))
 #    ct_gtw.add_sensor(CloudingThingsGroveGas('gas', period=5.0, pin=1))
 #    ct_gtw.add_sensor(CloudingThingsGroveLight('light', period=2.0, pin=2))
@@ -34,6 +33,9 @@ if __name__ == "__main__":
 #    ct_gtw.add_sensor(CloudingThingsGroveDht('dht', period=2.0, pin=5))
     ct_gtw.add_sensor(CloudingThingsGroveDhtPro('dht pro', pin=6))
     ct_gtw.add_sensor(CloudingThingsGroveSound('sound', pin=3))
+    ct_gtw.add_sensor(CloudingThingsGroveBarometerBmp085('barometer'))
+    ct_gtw.add_sensor(CloudingThingsGroveRfid('barometer'))
+
     #Start gateway
     ct_gtw.run()
 
